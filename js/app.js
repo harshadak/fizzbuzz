fe79b031508552607c1ad47935f2dc763413d796
Harshada Kurahatti <harshada.kurahatti@gmail.com>
2015-09-10 18:42:46 -0700

    Made changes to display the number list properly(1 and 2 were getting displayed on the same line).

--- js/app.js
+++ js/app.js
@@ -1,27 +1,32 @@
 function fizzBuzz() {
 	var num = $('#inputValue').val();
+	//var numList = $('.result .content');
 	var numList = $('#list');
 
 	for (var i = 1; i <= num; i++) {
-		if (i%15 == 0) {
-			numList.append('<p>FizzBuzz</p>');
-		} else if (i%5 == 0) {
-			numList.append('<p>Buzz</p>');
-		} else if (i%3 == 0) {
-			numList.append('<p>Fizz</p>');
+		if (i % 15 == 0) {
+			numList.append('<li class="answer">' + "FizzBuzz" + '</li>');
+			//numList.append('<p>FizzBuzz</p>');
+		} else if (i % 5 == 0) {
+			numList.append('<li class="answer">' + "Buzz" + '</li>');
+			//numList.append('<p>Buzz</p>');
+		} else if (i % 3 == 0) {
+			numList.append('<li class="answer">' + "Fizz" + '</li>');
+			//numList.append('<p>Fizz</p>');
 		} else {
-			numList.append(i);
+			numList.append('<li class="answer">' + i + '</li>');
+			//numList.append(i);
 		}
 	}
 
-	$('#inputValue').val('');
+	//$('#inputValue').val('');
 	//$('#list').('');
 }
 
 
 
 
-
 $(function() {
 	$('#calculate').on('click', fizzBuzz);
+	//numList.empty();
 });
